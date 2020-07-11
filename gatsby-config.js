76a1ec7c456adcd1951410e4c79091a74faad08d
Sam Chillcott <sam@samchillcott.com>
2020-07-11 12:19:19 +0100

    changed meta data to my name

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Developer Name',
+    title: 'Sam Chillcott',
     author: 'Martin Chammah',
     siteUrl: `https://www.martinchammah.dev`
   },

9c6843e22b043103e62e8660f46198792d2ff92f
Sam Chillcott <sam@samchillcott.com>
2020-07-11 11:18:03 +0100

    firebasic desc and screengrab

--- src/components/portfolio-items.js
+++ src/components/portfolio-items.js
@@ -19,23 +19,13 @@ const portfolioItems = [
     },
     {
         title: 'Portfolio Item 3',
-        sinopsis: 'It also includes a Blog section, with the createPages Api',
-        desc: 'Write posts directly in markdown. Thanks to plugins, and the createPage Api, gatbsy generates blog posts automatically',
-        img: './portfolio/ejs-blog.png',
+        sinopsis: 'Firebasic',
+        desc: 'Basic web app that utilizes Firebase Authentication, Storage, Cloud Firestore Database and Hosting.',
+        img: './portfolio/firebasic.png',
         repo: 'https://github.com/martin2844/ejs-blog',
-        live: 'https://my-ejs-blog.herokuapp.com/',
+        live: 'https://firebasic-b4186.firebaseapp.com/',
         idx: 3
-    },
-    {
-        title: 'Portfolio Item 4',
-        sinopsis: 'Its very minimalist, but that was the end goal',
-        desc: 'This was inspired in Dinesh Pandiyan personal page',
-        img: './portfolio/EJS.png',
-        repo: 'https://github.com/martin2844',
-        live: 'https://my-ejs-todo-list.herokuapp.com',
-        idx: 4
-    },
-
+    }
 ]
 
 export default portfolioItems

508f1566c60599b1785f5f761bb6a1aef1c157e3
Sam Chillcott <sam@samchillcott.com>
2020-07-24 13:09:44 +0100

    added helmet to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    `gatsby-plugin-react-helmet`
     "gatsby-transformer-remark",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
